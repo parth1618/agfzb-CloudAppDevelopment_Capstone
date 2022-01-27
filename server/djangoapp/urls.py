915ce1b9ff951125299abc6696efcc97c52236d7
parth1618 <parthjoshi.1618@gmail.com>
2022-01-27 11:51:58 +0000

    week2 - step1 v1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     # name the URL
     path(route='', view=views.index, name='index'),
     # path for about view
-    path(route='about', view=views.about, name='about'),
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-    path(route='contact', view=views.contact, name='contact'),
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
-
+    path('signup/', views.registration_request, name='signup'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
